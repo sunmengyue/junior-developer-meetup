7b6edee45751f0185154a1b8a39c4c169e3b3f5e
sunmengyue <mengyuesun@gmail.com>
2021-11-11 20:52:12 -0500

    pages and routing

---
+++ pages/about.js
@@ -0,0 +1,11 @@
+export default function AboutPage() {
+  return (
+    <div>
+      <h1>
+        This is an app for self-taught developers to meetup and gain expierence
+        in coding
+      </h1>
+      <p>Version: 1.0.0</p>
+    </div>
+  );
+}
---
+++ pages/events/add.js
@@ -0,0 +1,7 @@
+export default function AddPage() {
+  return (
+    <div>
+      <h1>Add events page</h1>
+    </div>
+  );
+}
