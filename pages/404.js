52a29545f97da1ba4b7be7e06c4380327cd96589
sunmengyue <mengyuesun@gmail.com>
2021-11-11 23:06:56 -0500

    add 404 page

---
+++ pages/404.js
@@ -0,0 +1,19 @@
+import Link from 'next/link';
+import Layout from '../components/Layout';
+import styles from '../styles/404.module.css';
+import { FaExclamationTriangle } from 'react-icons/fa';
+
+export default function NotFoundPage() {
+  return (
+    <Layout title="Page not found">
+      <div className={styles.error}>
+        <h1>
+          <FaExclamationTriangle />
+          <span>404</span>
+        </h1>
+        <h4>Sorry, there is nothing here</h4>
+        <Link href="/">Go back home</Link>
+      </div>
+    </Layout>
+  );
+}
